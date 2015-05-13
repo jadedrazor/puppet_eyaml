37088a5b850f09808928d43ff0147e8f2e5a720c
jadedrazor <bjones@jadedrazor.com>
2015-05-13 09:59:16 +0100

    initial commit, now with added files

---
+++ manifests/init.pp
@@ -0,0 +1,34 @@
+# Install and configure eyaml
+# keysdirectory is the directory the secure keys are stored in, /keys will be appended to whatever value is used
+
+class puppet_eyaml (
+  $keysdirectory = '/etc/puppet/secure/'){
+
+  
+  package {' ':
+    ensure => present,
+    provider => 'gem';
+  }
+
+
+  file { [ "$keysdirectory" ]:
+    ensure => directory,
+    recurse => 'true',
+    owner   => 'puppet',
+    group   => 'puppet',   
+    mode    => '0500'    
+  }
+
+  # This generates secure keys for the EYAML encryption.  If you want to use your own key to match any crypted content you'll need to replace these keys with your own.
+  exec {'generate-eyaml-keys':
+    path      => [ '/usr/bin', '/bin', '/usr/local/bin' ],
+    command   =>  "eyaml createkeys",
+    logoutput => on_failure,
+    creates   => "$keysdirectory/keys/private_key.pkcs7.pem",
+    cwd       => "$keysdirectory/",
+    timeout   => 0,
+    require   => [ File["$keysdirectory"], 
+                    Package['hiera-eyaml'] ]
+  }
+
+}
